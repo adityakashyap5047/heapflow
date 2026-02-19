421f0b0249e2445c81d333a92b9954e0ed6c46f0
adityakashyap5047 <adityakashyap5047@gmail.com>
2026-02-19 13:27:28 +0530

    updated the schema and components

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        pathname: "/adityakashyap5047/heapflow/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
